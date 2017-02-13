ceeb3b390f72b9934a674312de7aa8603f58afe2
lenn hypolite <lennhydesign@gmail.com>
2017-02-13 17:05:07 -0500

    Got the directive to display the d3 chart

---
+++ js/app/directives/chart.js
@@ -0,0 +1,27 @@
+function chart() {
+	return {
+    templateUrl: 'js/app/views/chart.html',
+		controller: 'WeatherController as vm'
+		// restrict: 'A',
+		// require: 'ngModel',
+		// link: function (scope, element, attrs, ngModel) {
+		// 	ngModel.$validators.multipleInput = function (values) {
+    //     var trueVal= true
+    //     for(var i =0; i < values.length; i++){
+		// 			console.log(values[i]);
+    //          if (values[i] === trueVal){
+    //            return true
+    //          }
+    //          else{
+    //            return false
+    //          }
+    //       }
+  	// 		};
+    //
+  	// 	}
+    };
+  }
+
+angular
+	.module('weatherApp')
+	.directive('chart', chart);
